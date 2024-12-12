3c1ed314cfe171f87516e2a6d9d1a6576ebac89e
suryasekhar2003 <143985884+suryasekhar2003@users.noreply.github.com>
2024-12-12 22:04:14 +0530

    new project

---
+++ models/user-model.js
@@ -0,0 +1,22 @@
+const mongoose = require ('mongoose')
+
+
+
+const userSchema = mongoose.Schema({
+    fullname: String,
+    email: String,
+    password:String,
+    cart: {
+        type:Array,
+        default:[]
+    },
+    orders:{
+            type:Array,
+            default:[]
+        },
+    isadmin: Boolean,
+    contact: Number,
+    picture: String,
+})
+
+module.exports = mongoose.model("user", userSchema)
