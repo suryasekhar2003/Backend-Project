f455bc7fcf098da30af01e3a6ad9bf4617f83f85
suryasekhar2003 <143985884+suryasekhar2003@users.noreply.github.com>
2024-12-16 19:31:30 +0530

    16 dec

--- config/mongoose-connectiono.js
+++ config/mongoose-connectiono.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose')
 const dbgr = require('debug')("development:mongoose")
+
+const config = require("config")
+
 mongoose
-.connect("mongodb://127.0.0.1:27017/scatch")
+.connect(`${config.get("MONGODB_URI")}/scatch`)
 .then(function(){
     dbgr("connected");
     
--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const router = express();
+const ownerModel = require("../models/owners-model")
+// const dotenv = require('dotenv')
+// dotenv.config()
 
 router.get('/', function(req, res){
     res.send('hey');
 })
+// 
+
+if(process.env.NODE === "development") {
+router.post("/create", function(req,res){
+    res.send("hey it's working")
+})
+}
+
 
 module.exports = router;
