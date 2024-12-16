1c19d5c0709dc3f8871533ee86ca67f6bf565de1
suryasekhar2003 <143985884+suryasekhar2003@users.noreply.github.com>
2024-12-16 19:31:11 +0530

    new

--- config/mongoose-connectiono.js
+++ config/mongoose-connectiono.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose')
-
+const dbgr = require('debug')("development:mongoose")
 mongoose
 .connect("mongodb://127.0.0.1:27017/scatch")
 .then(function(){
-    console.log("connected");
+    dbgr("connected");
     
 })
 .catch(function(err){
-    console.log(err);
+    dbgr(err);
     
 })
 
